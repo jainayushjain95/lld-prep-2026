0a3f5b6fd604ff91c284692fe350a0d92e874013
jainayushjain95 <jainayushjain95@gmail.com>
2026-09-19 16:48:53 +0530

    Add practice-one module for revising snake-and-ladders and tic-tac-toe

---
+++ practice-one/src/main/java/snakeladders/Main.java
@@ -0,0 +1,7 @@
+package snakeladders;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: implement Snake and Ladders game
+    }
+}
---
+++ practice-one/src/main/java/tictactoe/Main.java
@@ -0,0 +1,7 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: implement Tic Tac Toe game
+    }
+}
