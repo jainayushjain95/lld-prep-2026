c941839c869b3462f4d4732e182437e32aa8f0fa
jainayushjain95 <jainayushjain95@gmail.com>
2026-09-20 18:41:24 +0530

    Add User param to Playlist.addSong/addSongIfAbsent (WIP, Main not yet updated)

--- music-playlist-mixer/src/main/java/musicplaylistmixer/entities/Playlist.java
+++ music-playlist-mixer/src/main/java/musicplaylistmixer/entities/Playlist.java
@@ -4,36 +4,45 @@ import musicplaylistmixer.observers.PlaylistObserver;
 import musicplaylistmixer.services.sort.SortService;
 import musicplaylistmixer.utilities.CommonUtility;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class Playlist {
+
     private String name;
     private final List<PlaylistEntry> playlistEntries;
     private final User owner;
     private final Set<User> collaborators;
     private final List<PlaylistObserver> observers;
 
+
     public Playlist(String name, User owner) {
-        if(owner == null) {
+        if (owner == null) {
             throw new IllegalArgumentException("Playlist cant exist without owner");
         }
-        if(CommonUtility.isBlank(name)) {
+        if (CommonUtility.isBlank(name)) {
             throw new IllegalArgumentException("name cant be empty");
         }
+
         this.observers = new ArrayList<>();
         this.collaborators = new HashSet<>();
         this.owner = owner;
         this.playlistEntries = new ArrayList<>();
         this.name = name;
+        this.collaborators.add(owner);
     }
 
     public void addObserver(PlaylistObserver observer) {
-        if(observer == null) {
+        if (observer == null) {
             throw new IllegalArgumentException("Observer cant be null");
         }
         observers.add(observer);
     }
-
 
     public boolean removeObserver(PlaylistObserver observer) {
         return observers.remove(observer);
@@ -43,46 +52,56 @@ public class Playlist {
         return new ArrayList<>(collaborators);
     }
 
-    public boolean addCollaborators(User user) {
-        if(user == null) {
+    public boolean addCollaborator(User user) {
+        if (user == null) {
             throw new IllegalArgumentException("User cant be null");
         }
         return collaborators.add(user);
     }
 
-    public void addSong(Song song) {
-        if(song == null) {
+    public boolean removeCollaborator(User user) {
+        if (user == null || user.equals(owner)) {
+            return false;
+        }
+        return collaborators.remove(user);
+    }
+
+    public void addSong(Song song, User addedBy) {
+        addSong(song, null, addedBy);
+    }
+
+    public void addSong(Song song, String nickname, User addedBy) {
+        if (song == null) {
             throw new IllegalArgumentException("Non existent Song cant be added");
         }
-        addSong(song, null);
-    }
-
-    public void addSong(Song song, String nickname) {
-        playlistEntries.add(new PlaylistEntry(song, nickname));
-    }
-
-    public boolean addSongIfAbsent(Song song) {
-        if(!hasSong(song)) {
-            addSong(song);
-            return true;
+        if (addedBy == null) {
+            throw new IllegalArgumentException("addedBy cant be null");
         }
-        return false;
+        if (!canAdd(addedBy)) {
+            throw new IllegalArgumentException(addedBy.getName() + " is not allowed to add to " + name);
+        }
+        playlistEntries.add(new PlaylistEntry(song, nickname, addedBy));
+        notifySongAdded(song, addedBy);
     }
 
-    public boolean addSongIfAbsent(Song song, String nickname) {
-        if(!hasSong(song)) {
-            addSong(song, nickname);
-            return true;
+    public boolean addSongIfAbsent(Song song, User addedBy) {
+        return addSongIfAbsent(song, null, addedBy);
+    }
+
+    public boolean addSongIfAbsent(Song song, String nickname, User addedBy) {
+        if (hasSong(song)) {
+            return false;
         }
-        return false;
+        addSong(song, nickname, addedBy);
+        return true;
     }
 
     public boolean removeSong(Song song) {
         boolean removed = false;
         Iterator<PlaylistEntry> iterator = playlistEntries.iterator();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             PlaylistEntry playlistEntry = iterator.next();
-            if(playlistEntry.getSong().equals(song)) {
+            if (playlistEntry.getSong().equals(song)) {
                 iterator.remove();
                 removed = true;
             }
@@ -100,7 +119,7 @@ public class Playlist {
 
     public List<Song> getSongs() {
         List<Song> songs = new ArrayList<>();
-        for(PlaylistEntry playlistEntry : playlistEntries) {
+        for (PlaylistEntry playlistEntry : playlistEntries) {
             songs.add(playlistEntry.getSong());
         }
         return songs;
@@ -110,25 +129,37 @@ public class Playlist {
         return name;
     }
 
+    public User getOwner() {
+        return owner;
+    }
+
     public void rename(String name) {
-        if(CommonUtility.isBlank(name)) {
+        if (CommonUtility.isBlank(name)) {
             throw new IllegalArgumentException("name cant be empty");
         }
         this.name = name;
-    }
-
-    private boolean hasSong(Song song) {
-        for(PlaylistEntry playlistEntry : playlistEntries) {
-            if(playlistEntry.getSong().equals(song)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     public List<Song> sortedBy(Comparator<Song> comparator) {
         return SortService.sort(this.getSongs(), comparator);
     }
 
+    private boolean canAdd(User user) {
+        return collaborators.contains(user);
+    }
 
+    private void notifySongAdded(Song song, User addedBy) {
+        for (PlaylistObserver observer : observers) {
+            observer.onSongAdded(this, song, addedBy);
+        }
+    }
+
+    private boolean hasSong(Song song) {
+        for (PlaylistEntry playlistEntry : playlistEntries) {
+            if (playlistEntry.getSong().equals(song)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- music-playlist-mixer/src/main/java/musicplaylistmixer/entities/PlaylistEntry.java
+++ music-playlist-mixer/src/main/java/musicplaylistmixer/entities/PlaylistEntry.java
@@ -5,13 +5,15 @@ public class PlaylistEntry {
     private final Song song;
     private final LocalDateTime addedAt;
     private String nickname;
+    private final User addedBy;
 
-    public PlaylistEntry(Song song) {
-        this(song, null);
+    public PlaylistEntry(Song song, User addedBy) {
+        this(song, null, addedBy);
     }
 
-    public PlaylistEntry(Song song, String nickname) {
+    public PlaylistEntry(Song song, String nickname, User addedBy) {
         this.song = song;
+        this.addedBy = addedBy;
         this.addedAt = LocalDateTime.now();
         this.nickname = nickname;
     }
