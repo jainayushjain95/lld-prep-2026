e91880d3df5fe01e3b0d0d4e8273b4d2ae671849
jainayushjain95 <jainayushjain95@gmail.com>
2026-09-20 18:17:32 +0530

    Add Library entity and PlayStrategyFactory

---
+++ music-playlist-mixer/src/main/java/musicplaylistmixer/entities/Library.java
@@ -0,0 +1,32 @@
+package musicplaylistmixer.entities;
+
+import musicplaylistmixer.services.sort.SortService;
+import musicplaylistmixer.utilities.CommonUtility;
+
+import java.util.*;
+
+public class Library {
+    private final Map<String, Song> songs;
+
+    public Library() {
+        this.songs = new LinkedHashMap<>();
+    }
+
+
+    public void addSong(Song song) {
+        if(song == null) {
+            throw new IllegalArgumentException("Song cant be null");
+        }
+        songs.put(song.getId(), song);
+    }
+
+
+    public Song findById(String id) {
+        return songs.get(id);
+    }
+
+
+    public List<Song> getAllSongs() {
+        return new ArrayList<>(songs.values());
+    }
+}
---
+++ music-playlist-mixer/src/main/java/musicplaylistmixer/services/play/PlayStrategyFactory.java
@@ -0,0 +1,23 @@
+package musicplaylistmixer.services.play;
+
+import musicplaylistmixer.constants.Mode;
+
+import java.util.Random;
+
+public class PlayStrategyFactory {
+    public static PlayStrategy create(Mode mode, Random random) {
+        if(mode.equals(Mode.Sequential)) {
+            return new PlaySequentialStrategy();
+        }
+        if(mode.equals(Mode.SHUFFLE)) {
+            return new PlayShuffleStrategy(random);
+        }
+        if(mode.equals(Mode.REPEAT_ONE)) {
+            return new PlayRepeatOneStrategy();
+        }
+        if(mode.equals(Mode.REPEAT_ALL)) {
+            return new PlayRepeatAllStrategy();
+        }
+        return null;
+    }
+}
